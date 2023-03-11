e07bae94adce72be292016b753afd319a7373f60
João Marcos Kaminoski de Souza <kamisou.dev@gmail.com>
2023-03-11 16:48:59 -0300

    added execution messages on the install script

--- Install.ps1
+++ Install.ps1
@@ -1,3 +1,6 @@
 $destinationPath = "$($PSHOME.Split(';')[0])/Modules"
 
+Write-Host "Copying Search-Scoop module to `$PSHOME folder..." -ForegroundColor Black
 Copy-Item ".\Search-Scoop\" $destinationPath -Recurse -Force
+
+Write-Host "Done! Reset the terminal to load the module."
